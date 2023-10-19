1ce27c8856e68caaabb26c5d3ddb5f40a2511085
Marc <marcdelalcazar@gmail.com>
2023-10-19 08:53:18 +0200

    New tests

--- setup.py
+++ setup.py
@@ -14,5 +14,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["numpy", "scipy", "math"],
+    install_requires=["numpy", "scipy"],
 )
